3c4a01ecf2bcadc430625d818c098295fa7efd52
marsenzakirov <48007040+marsenzakirov@users.noreply.github.com>
2022-03-02 21:34:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const {MongoClient} = require("mongodb")
 const cron = require('node-cron');
 let shell = require('shelljs')
 
-const client = new MongoClient()
-const token = ""
+const client = new MongoClient(clientMongoDB)
+const token = tokenTelegram
 const bot = new TelegramAPI(token, {polling: true})
 
 
